8fb53519a35250d217bfb1eae5296814db58f7f4
Kirill Shnyrev <k.shnyrev@mail.ru>
2021-03-15 15:54:51 +0300

    added some structure

---
+++ board.py
@@ -0,0 +1,32 @@
+class Board:
+    # создание поля
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+        self.board = [[0] * width for _ in range(height)]
+        # значения по умолчанию
+        self.left = 10
+        self.top = 10
+        self.cell_size = 30
+
+    # настройка внешнего вида
+    def set_view(self, left, top, cell_size):
+        self.left = left
+        self.top = top
+        self.cell_size = cell_size
+
+    def get_click(self, mouse_pos):
+        cell = self.get_cell(mouse_pos)
+        self.on_click(cell)
+
+    def get_cell(self, mouse_pos):
+        x = mouse_pos[0] - self.left
+        y = mouse_pos[1] - self.top
+        if x < 0 or x > self.width * self.cell_size:
+            return None
+        if y < 0 or y > self.height * self.cell_size:
+            return None
+        return y // self.cell_size, x // self.cell_size
+
+    def on_click(self, cell_coords):
+        pass
